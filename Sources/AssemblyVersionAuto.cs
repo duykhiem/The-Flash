d7c6a4ec89a5af1b86e2be68d5278cd13fe826e3
Khiem Nguyen Duy <duykhiem.it@gmail.com>
2019-01-14 20:35:40 +0700

    Add Episerver Quicksilver

---
+++ Sources/AssemblyVersionAuto.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
+[assembly: AssemblyVersion("12.15.0.0")]
+[assembly: AssemblyInformationalVersion("12.15.0")]
+[assembly: AssemblyFileVersion("12.15.0.3485")]
